6a091f737808c7f5bb1f9aef3ded125044626bfa
ShimmyShong <lattabun@gmail.com>
2023-08-17 00:34:51 -0400

    commit

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -82,7 +82,6 @@ export default function Projects() {
                                 </div>
                             </div>
                         </>
-
                     )
                 })}
             </div>
